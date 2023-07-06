3e0a273a6fc62031a18315c32dc619eab0f2b34b
Hardmand <javt28@outlook.com>
2023-07-06 13:07:13 -0500

    primer contracto con ethers.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { ethers } from 'ethers'
+import contractABI from './SmartContract/ABI.json'
+
+const contractAddress = '0x12B058a406a9C2214F62Ffa9bFF6b1c370446293' //300 BSC -- 293 MUMBAI
+
+const loadData = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum)
+    const contract = new ethers.Contract(contractAddress, contractABI, provider)
+    const balance = await contract.getBalance()
+    alert(balance)
+}
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -22,8 +35,9 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Change <code>src/App.jsx</code> and save to test HMR
         </p>
+        <button onClick={loadData}>Click Me</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
